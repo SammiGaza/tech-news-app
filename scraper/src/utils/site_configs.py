c82adcdda759655516212a00bb82d6fa7af2eda5
SavviSam <swghazzawi@gmail.com>
2024-01-10 00:12:11 -0700

    add XPaths to TechCrunch site

--- scraper/src/utils/site_configs.py
+++ scraper/src/utils/site_configs.py
@@ -1,12 +1,16 @@
 SITE_CONFIGS = {
   'TechCrunch': {
     'url': 'https://techcrunch.com/',
-    'article_list_xpath': '',
-    'title_xpath': '',
-    'author_xpath': '',
-    'image_xpath': '',
-    'article_url_xpath': '',
-    'date_xpath': '',
+    'article_list_xpath': '//*[@id="tc-main-content"]/div[3]/div/div/div',
+    
+    # 'a' link element stores title as its text
+    'title_xpath': '//*[@id="tc-main-content"]/div[3]/div/div/div/article/header/h2/a',
+    # 'a' link element stores authors as its text
+    'author_xpath': '//*[@id="tc-main-content"]/div[3]/div/div/div/article/header/div[2]/div/span/span/a', 
+
+    'image_xpath': '//*[@id="tc-main-content"]/div[3]/div/div/div/article/footer/figure/picture/img/@src',
+    'article_url_xpath': '//*[@id="tc-main-content"]/div[3]/div/div/div/article/header/h2/a/@href',
+    'date_xpath': '//*[@id="tc-main-content"]/div[3]/div/div/div/article/header/div[2]/div/div/time/@datetime',
   },
   'Wired': {
     'url': 'https://www.wired.com/',
@@ -45,3 +49,5 @@ SITE_CONFIGS = {
     'date_xpath': '',
   },
 }
+
+
