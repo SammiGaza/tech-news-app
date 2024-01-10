815c190683f0f4074336e1c772282e070cac266b
SavviSam <swghazzawi@gmail.com>
2024-01-09 23:19:41 -0700

    Restructure folders

---
+++ scraper/src/main_scraper.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from datetime import time
+from pprint import pprint
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
